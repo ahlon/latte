74abba0f2ca1d8e1381a6f565a6a73c8aa73e3b8
ahlon <ahlon2002@gmail.com>
2016-11-16 17:38:35 +0800

    disable csrf

--- src/main/java/com/redq/latte/conf/WebSecurityConfig.java
+++ src/main/java/com/redq/latte/conf/WebSecurityConfig.java
@@ -24,6 +24,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .logout()
                 .permitAll();
+        
+        http
+    	.csrf().disable();
     }
 
     @Autowired
